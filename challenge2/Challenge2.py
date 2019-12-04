0bc236dc6516b183e4039f80b803837738e86273
surph3r <grant/.turpin@gmail.com>
2019-12-04 15:08:53 -0800

    Completed challenge

--- challenge2/Challenge2.py
+++ challenge2/Challenge2.py
@@ -1,10 +1,12 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-
-class Challenge1(unittest.TestCase):
+class Challenge2(unittest.TestCase):
 
     def setUp(self):
         # code to start webdriver
@@ -14,15 +16,18 @@ class Challenge1(unittest.TestCase):
         # code to close webdriver
         self.driver.close()
 
-    def test_challenge1(self):
+    def test_challenge2(self):
         # code for our test steps
-        self.driver.get("https://www.google.com")
-        self.assertIn("Google", self.driver.title)
+        self.driver.get("https://www.copart.com")
+        self.assertIn("Copart", self.driver.title)
         searchField = self.driver.find_element(By.ID, "input-search")
+        searchField.clear()
         searchField.click()
-        searchField.text("porsche")
-        searchButton = self.driver.find_element(By.XPATH, "//*[@id="search-form"]/div/div[2]/button")
+        searchField.send_keys("exotics")
+        searchField.send_keys(Keys.ENTER)
+        element = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.ID, "serverSideDataTable")))
+        self.assertIn("PORSCHE", self.driver.page_source)
 
 
 if __name__ == '__main__':
-        unittest.main()
+    unittest.main()
