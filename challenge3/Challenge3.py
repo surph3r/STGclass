7c924bc524defcb5e11b65e583ddb2883d74de29
surph3r <grant/.turpin@gmail.com>
2019-12-05 20:06:55 -0800

    Added for and while loop function defs to Challenge3

--- challenge3/Challenge3.py
+++ challenge3/Challenge3.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class Challenge3(unittest.TestCase)
+class Challenge3(unittest.TestCase):
 
     def setUp(self):
         # code to start webdriver
@@ -16,10 +16,28 @@ class Challenge3(unittest.TestCase)
         # code to close webdriver
         self.driver.close()
 
-    def test_challenge3(self):
+    def test_challenge3_forloop(self):
         # code for our test steps
         self.driver.get("https://www.copart.com")
         self.assertIn("Copart", self.driver.title)
+        WebDriverWait(self.driver,1)
+        popularSearches = self.driver.find_elements(By.XPATH, '//*[@id="tabTrending"]/div[1]//a')
+        popularSearches[0].get_attribute()
+        print(popularSearches)
+        # getThirdListItem.get_attribute(self.driver,"a")
+
+    def test_challenge3_whileloop(self):
+        # code for our test steps
+        self.driver.get("https://www.copart.com")
+        self.assertIn("Copart", self.driver.title)
+        WebDriverWait(self.driver,1)
+        popularSearches = self.driver.find_elements(By.XPATH, '//*[@id="tabTrending"]/div[1]//a')
+        while(i++):
+            if popularSearches[i]:
+                pass
+            else:
+                break
+        # getThirdListItem.get_attribute(self.driver,"a")
 
 
 if __name__ == '__main__':
