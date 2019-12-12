815662492f132cb55d8f193ea0fd85e8e9b235e9
surph3r <grant/.turpin@gmail.com>
2019-12-12 15:20:57 -0800

    Split code into two test cases. Added teacher's version of the for loop for comparison

--- challenge3/Challenge3.py
+++ challenge3/Challenge3.py
@@ -11,19 +11,15 @@ class Challenge3(unittest.TestCase):
     def setUp(self):
         # code to start webdriver
         self.driver = webdriver.Chrome("../chromedriver.exe")
+        self.driver.get("https://www.copart.com")
+        self.assertIn("Copart", self.driver.title)
+        WebDriverWait(self.driver, 1) # give it a sec for stuff to load
 
     def tearDown(self):
         # code to close webdriver
         self.driver.close()
 
-    def test_challenge3(self):
-        # code for our test steps
-        self.driver.get("https://www.copart.com")
-        self.assertIn("Copart", self.driver.title)
-
-        # Let's pause for station identification
-        WebDriverWait(self.driver, 1)
-
+    def test_challenge3_for_loop(self):
         popularSearches = self.driver.find_elements(By.XPATH, '//*[@id="tabTrending"]/div[1]/div[2]//a')
         numSearches = len(popularSearches)
 
@@ -31,11 +27,18 @@ class Challenge3(unittest.TestCase):
         for item in range(numSearches):
             print(popularSearches[item].text + " - " + popularSearches[item].get_attribute('href'))
 
+        print("\n" + "Teacher's version of the FOR Loop:")
+        elements = self.driver.find_elements(By.XPATH, '//*[@id="tabTrending"]/div[1]//a')
+        for count in elements:
+            print(count.text + ': ' + count.get_attribute("href"))
+
+    def test_challenge3_while_loop(self):
+        popularSearches = self.driver.find_elements(By.XPATH, '//*[@id="tabTrending"]/div[1]/div[2]//a')
+        numSearches = len(popularSearches)
+
         print("\n" + "WHILE Loop iteration:")
         i = 0
         while i < numSearches:
-            if i == 20:
-                break
             print(popularSearches[i].text + " - " + popularSearches[i].get_attribute('href'))
             i+=1
 
