9fea9ba2d64a6019f752cda82c3ce9e02eb6b620
surph3r <grant/.turpin@gmail.com>
2019-12-12 18:39:26 -0800

    Created word lists

--- challenge4/convertNumToString.py
+++ challenge4/convertNumToString.py
@@ -1,3 +1,9 @@
 def convertNumToString(num):
+
+    one_twenty = ["", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine ", "ten ", "eleven ",
+            "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen "]
+    twenties = ["", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "]
+    thousands = ["", "thousand ", "million ", "billion ", "trillion ", "quadrillion "]
+
     words = str(num)
     return words
