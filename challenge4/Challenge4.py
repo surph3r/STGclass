13dc2dcb6f1bbd4e71292beb590f213d67a94072
surph3r <grant/.turpin@gmail.com>
2019-12-12 19:40:48 -0800

    Renamed several vars. Created list of numbers to process with fibonacci function

--- challenge4/Challenge4.py
+++ challenge4/Challenge4.py
@@ -20,15 +20,20 @@ class Challenge4(unittest.TestCase):
 
     def test_challenge4(self):
         # code for our test steps
-        my_num = 21
-        my_ord_num = num2words(my_num, ordinal=True)
-        my_ord_num = my_ord_num.replace('-', ' ')
+        my_nums = [9, 13, 144, 7408, 9876543210]
+        my_num_ords = []
+        for i in range(len(my_nums)):
+            item_ord = num2words(my_nums[i], ordinal=True)
+            item_ord = item_ord.replace('-', ' ')
+            item_ord = item_ord.replace('and ','')
+            my_num_ords.append(item_ord)
 
         fib_num = fibonacci(my_num)
+        fib_num_words = fibonacci(fib_num)
         word_salad = num2words(fib_num)
         word_salad = word_salad.replace('-', ' ')
 
-        print('The ' + my_ord_num + ' Fibonacci number is: ' + str(fib_num) + ' - ' + word_salad)
+        print('The ' + my_num_ord + ' Fibonacci number is: ' + str(fib_num) + ' - ' + word_salad)
 
 
 if __name__ == '__main__':
